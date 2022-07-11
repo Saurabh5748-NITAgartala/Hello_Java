a75f3b21d85f57457d77391175f2f197f6834dc7
Saurabh5748-NITAgartala <89444201+Saurabh5748-NITAgartala@users.noreply.github.com>
2022-07-11 15:46:20 +0530

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,32 @@
+package pkg1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello World");
+byte b1=12;
+short s1=234;
+int i1=1234;
+long l1=455553;
+float f1=3.984f;
+double d1=2.5645453535;
+char ch1='s';
+String st1="saurabh";
+boolean bl1=true;
+System.out.println(bl1);
+System.out.println(s1);
+System.out.println(b1);
+System.out.println(i1);
+System.out.println(l1);
+System.out.println(f1);
+System.out.println(d1);
+System.out.println(ch1);
+System.out.println(st1);
+
+
+
+
+	}
+
+}
